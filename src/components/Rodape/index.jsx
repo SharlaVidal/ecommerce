2a37803855bf223f5ab22519236468efa3eb6233
SharlaVidal <144283288+SharlaVidal@users.noreply.github.com>
2024-07-23 21:52:19 -0300

    adicionando componentes

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Rodape = () => {
+  return (
+    <footer className="text-center bg-black text-bg-dark">
+      <p className="card-text py-3">
+        2024 <i className="bi bi-c-circle"></i> Desenvolvido por Sharla Vidal |
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;
